fecebab68e1e1197f374776e49da96756984b023
Ivan Goncharov <ivan.stereotekk@gmail.com>
2024-04-16 23:29:08 +0300

    Add files via upload

---
+++ pyinitor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo Hello This Is - PYINITOR !
+read -p 'Project name:' pname
+echo Pyinitor creates project... $pname
+mkdir $pname && cd $pname
+python3 -m venv venv
+source venv/bin/activate
+touch main.py && touch requirements.txt
+code main.py
+
